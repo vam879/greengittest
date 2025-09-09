abf066fa3797e78476541605ca9e3d42dbb3827d
wjsdydwns01 <113407599+weasdf12@users.noreply.github.com>
2025-09-09 14:26:15 +0900

    introduce

---
+++ src/main/java/controller/introduce/entrance/NoticeController.java
@@ -0,0 +1,52 @@
+package controller.introduce.entrance;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/#.do")
+public class NoticeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	private ArticleService articleService = ArticleService.INSTANCE;	
+	
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+
+		// 요청 페이지 번호 수신
+		String pg = req.getParameter("pg");
+				
+		// 페이지네이션 처리 요청
+		PagenationDTO pagenationDTO = articleService.getPagenationDTO(pg);
+		
+		// 글 목록 조회
+		int start = pagenationDTO.getStart();
+		List<ArticleDTO> dtoList = articleService.findAll(start);
+		
+		// request 공유참조(JSP 출력)
+		req.setAttribute("dtoList", dtoList);
+		req.setAttribute("pagenationDTO", pagenationDTO);		
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/#.jsp");
+		dispatcher.forward(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
---
+++ src/main/java/controller/introduce/hi/OrgController.java
@@ -0,0 +1,34 @@
+package controller.introduce;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//수정:		전용준	
+//내용:		조직도
+//수정일:		25.0909
+			
+
+
+
+@WebServlet("/1대학소개/대학소개4-조직도.do")
+public class OrgController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/1대학소개/대학소개4-조직도.jsp");
+		dispatcher.forward(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
