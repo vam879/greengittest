b71794aa0dae11cc2879a020728f70a44345e905
whdqhd4149 <whdqhd4149@gmail.com>
2025-09-09 09:09:53 +0900

    dto

---
+++ src/main/java/util/dto/ProfessorDTO.java
@@ -0,0 +1,178 @@
+package dto;
+
+public class ProfessorDTO {
+	
+	private int pro_no;
+	private String pro_jumin;
+	private String pro_name;
+	private String pro_eng_name;
+	private String pro_gen;
+	private String pro_nation;
+	private String pro_hp;
+	private String pro_email;
+	private String pro_addr;
+	private String pro_univ;
+	private String pro_grad_date;
+	private String pro_degree;
+	private String pro_appint_date;
+	private String pro_position;
+	private String pro_status;
+	private int pro_seq;
+	
+	private int dep_no;
+
+	
+	
+	
+	public int getDep_no() {
+		return dep_no;
+	}
+
+	public void setDep_no(int dep_no) {
+		this.dep_no = dep_no;
+	}
+	
+
+	public int getPro_no() {
+		return pro_no;
+	}
+
+	public void setPro_no(int pro_no) {
+		this.pro_no = pro_no;
+	}
+
+	public String getPro_jumin() {
+		return pro_jumin;
+	}
+
+	public void setPro_jumin(String pro_jumin) {
+		this.pro_jumin = pro_jumin;
+	}
+
+	public String getPro_name() {
+		return pro_name;
+	}
+
+	public void setPro_name(String pro_name) {
+		this.pro_name = pro_name;
+	}
+
+	public String getPro_eng_name() {
+		return pro_eng_name;
+	}
+
+	public void setPro_eng_name(String pro_eng_name) {
+		this.pro_eng_name = pro_eng_name;
+	}
+
+	public String getPro_gen() {
+		return pro_gen;
+	}
+
+	public void setPro_gen(String pro_gen) {
+		this.pro_gen = pro_gen;
+	}
+
+	public String getPro_nation() {
+		return pro_nation;
+	}
+
+	public void setPro_nation(String pro_nation) {
+		this.pro_nation = pro_nation;
+	}
+
+	public String getPro_hp() {
+		return pro_hp;
+	}
+
+	public void setPro_hp(String pro_hp) {
+		this.pro_hp = pro_hp;
+	}
+
+	public String getPro_email() {
+		return pro_email;
+	}
+
+	public void setPro_email(String pro_email) {
+		this.pro_email = pro_email;
+	}
+
+	public String getPro_addr() {
+		return pro_addr;
+	}
+
+	public void setPro_addr(String pro_addr) {
+		this.pro_addr = pro_addr;
+	}
+
+	public String getPro_univ() {
+		return pro_univ;
+	}
+
+	public void setPro_univ(String pro_univ) {
+		this.pro_univ = pro_univ;
+	}
+
+	public String getPro_grad_date() {
+		return pro_grad_date;
+	}
+
+	public void setPro_grad_date(String pro_grad_date) {
+		this.pro_grad_date = pro_grad_date;
+	}
+
+	public String getPro_degree() {
+		return pro_degree;
+	}
+
+	public void setPro_degree(String pro_degree) {
+		this.pro_degree = pro_degree;
+	}
+
+	public String getPro_appint_date() {
+		return pro_appint_date;
+	}
+
+	public void setPro_appint_date(String pro_appint_date) {
+		this.pro_appint_date = pro_appint_date;
+	}
+
+	public String getPro_position() {
+		return pro_position;
+	}
+
+	public void setPro_position(String pro_position) {
+		this.pro_position = pro_position;
+	}
+
+	public String getPro_status() {
+		return pro_status;
+	}
+
+	public void setPro_status(String pro_status) {
+		this.pro_status = pro_status;
+	}
+
+	public int getPro_seq() {
+		return pro_seq;
+	}
+
+	public void setPro_seq(int pro_seq) {
+		this.pro_seq = pro_seq;
+	}
+
+	@Override
+	public String toString() {
+		return "ProfessorDTO [pro_no=" + pro_no + ", pro_jumin=" + pro_jumin + ", pro_name=" + pro_name
+				+ ", pro_eng_name=" + pro_eng_name + ", pro_gen=" + pro_gen + ", pro_nation=" + pro_nation + ", pro_hp="
+				+ pro_hp + ", pro_email=" + pro_email + ", pro_addr=" + pro_addr + ", pro_univ=" + pro_univ
+				+ ", pro_grad_date=" + pro_grad_date + ", pro_degree=" + pro_degree + ", pro_appint_date="
+				+ pro_appint_date + ", pro_position=" + pro_position + ", pro_status=" + pro_status + ", pro_seq="
+				+ pro_seq + "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/util/dto/UserDTO.java
@@ -0,0 +1,58 @@
+package dto;
+
+public class UserDTO {
+	
+	private int us_id;
+	private String us_pass;
+	private String us_name;
+	private String us_hp;
+	private String us_email;
+	private String us_addr;
+	
+	public int getUs_id() {
+		return us_id;
+	}
+	public void setUs_id(int us_id) {
+		this.us_id = us_id;
+	}
+	public String getUs_pass() {
+		return us_pass;
+	}
+	public void setUs_pass(String us_pass) {
+		this.us_pass = us_pass;
+	}
+	public String getUs_name() {
+		return us_name;
+	}
+	public void setUs_name(String us_name) {
+		this.us_name = us_name;
+	}
+	public String getUs_hp() {
+		return us_hp;
+	}
+	public void setUs_hp(String us_hp) {
+		this.us_hp = us_hp;
+	}
+	public String getUs_email() {
+		return us_email;
+	}
+	public void setUs_email(String us_email) {
+		this.us_email = us_email;
+	}
+	public String getUs_addr() {
+		return us_addr;
+	}
+	public void setUs_addr(String us_addr) {
+		this.us_addr = us_addr;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserDTO [us_id=" + us_id + ", us_pass=" + us_pass + ", us_name=" + us_name + ", us_hp=" + us_hp
+				+ ", us_email=" + us_email + ", us_addr=" + us_addr + "]";
+	}
+	
+	
+	
+
+}
