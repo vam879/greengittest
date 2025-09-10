ed01dc60f3a6b6eec900065bbc4821f76234902c
wjsdydwns01 <113407599+weasdf12@users.noreply.github.com>
2025-09-10 14:52:52 +0900

    컨트롤러 완

---
+++ src/main/java/controller/introduce/IdeologyController.java
@@ -0,0 +1,31 @@
+package controller.introduce;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//수정:		전용준	
+//내용:		교육이념
+//수정일:		25.0909
+
+@WebServlet("/introduce/ideology.do")
+public class IdeologyController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/introduce/ideology.jsp");
+		dispatcher.forward(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
---
+++ src/main/java/controller/introduce/OrganizationController.java
@@ -0,0 +1,28 @@
+package controller.introduce;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/introduce/organization.do")
+public class OrganizationController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/introduce/organization.jsp");
+		dispatcher.forward(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
